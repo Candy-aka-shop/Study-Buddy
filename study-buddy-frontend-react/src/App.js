8843196ad34272e46a6ad60269409887b4b8e82d
Jema <sdjemma@gmail.com>
2025-04-22 18:54:55 +0200

    Update commit

--- study-buddy-frontend-react/src/App.js
+++ study-buddy-frontend-react/src/App.js
@@ -4,13 +4,15 @@ import logo from './logo.svg';
 import './App.css';
 
 function App() {
-    return (
-        <div className="App">
-            <header className="App-header">
-                <img src={logo} className="App-logo" alt="logo" />
-                <h1>Study Buddy App - User Registration</h1>
-                <RegisterForm /> {/* Display RegisterForm component */}
-            </header>
-        </div>
-    );
+  return (
+    <div className="App">
+      <header className="App-header">
+        <img src={logo} className="App-logo" alt="logo" />
+        <h1>Study Buddy App - User Registration</h1>
+        <RegisterForm /> {/* Display RegisterForm component */}
+      </header>
+    </div>
+  );
 }
+
+export default App;
